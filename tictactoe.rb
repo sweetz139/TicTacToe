2b1865df8316b37f9af74988020cf923b4f35f16
Zachary Sweet <ztsc97@mst.edu>
2021-01-30 21:18:37 -0600

    Added the Player class

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,1 +1,9 @@
 #Script will allow you to play either with a computer or a second player
+#Added the Player class 01/30/2021
+class player(username)
+	#player should be able to move 
+	#player should have a username 
+	@username = username
+	@move = move
+	
+end
